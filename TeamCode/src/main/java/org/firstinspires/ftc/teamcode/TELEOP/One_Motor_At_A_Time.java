8ab40ba0ece28039cfe4b156c68b5670b7468818
Your Name <aminesakoute288@gmail.com>
2026-01-17 11:14:42 +0100

    Yeah, apparently I hadn't update the gearbox ratio of the expulsion motors. I also simplified this bc no need for every variable in the book + I added on-the-go tuning of the RPM amount + added telemetry.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TELEOP/One_Motor_At_A_Time.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TELEOP/One_Motor_At_A_Time.java
@@ -5,9 +5,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp
 public class One_Motor_At_A_Time extends LinearOpMode {
+
+    public void init(HardwareMap hwMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotorEx rightExpulsionMotor = hardwareMap.get(DcMotorEx.class, "REM");
@@ -24,64 +32,36 @@ public class One_Motor_At_A_Time extends LinearOpMode {
 
         // CONSTANTS at initialization
         double BASE_TICKS_PER_REV_DC = 28;
-        double BASE_TICKS_PER_REV_CORE = 288;
-        double DT_MOTOR_RPM = 6000.0;
-        double INTAKE_MOTOR_RPM = 125;
-        double RAMP_MOTOR_RPM = 6000.0;
-        double EXPULSION_MOTOR_RPM = 6000.0;
-        double DT_GEARBOX_RATIO = 4.0 * 5.0;
-        double INTAKE_GEARBOX_RATIO = 1.0;
-        double RAMP_MOTOR_GEARBOX_RATIO = 3.0;
-        double EXPULSION_MOTOR_GEARBOX_RATIO = 3.0;
+        double MAX_EXPULSION_MOTOR_RPM = 6000.0;
+        double EXPULSION_MOTOR_GEARBOX_RATIO = 1.0;
 
         double RPM_to_RPS = 1 / 60.0;
 
-        double DT_SPEED = 1.0;
-        double Strafing_Correction = 1.12;
+        double wantedRPM = 3000.0;
 
         waitForStart();
 
         if (isStopRequested()) return;
 
-        while (opModeIsActive()) {
-            // Gets the speed you wanna go at
-            boolean quarterSpeed = gamepad1.x;
-            boolean halfSpeed = gamepad1.a;
-            boolean threeSpeed = gamepad1.b;
-            boolean fullSpeed = gamepad1.y;
-
-            if (quarterSpeed) {
-                DT_SPEED = 0.25;
+        while (opModeIsActive()){
+            if(gamepad1.a){
+                wantedRPM += 100;
             }
-            if (halfSpeed) {
-                DT_SPEED = 0.50;
+            if(gamepad1.b){
+                wantedRPM -=100;
             }
-            if (threeSpeed) {
-                DT_SPEED = 0.75;
+            if(gamepad1.x){
+                wantedRPM += 10;
             }
-            if (fullSpeed) {
-                DT_SPEED = 1.00;
+            if(gamepad1.y){
+                wantedRPM -= 10;
             }
 
-            // Drivetrain Controller Controls
-            double y = -gamepad1.left_stick_y * DT_SPEED;
-            double x = gamepad1.left_stick_x * Strafing_Correction * DT_SPEED;
-            double rx = -gamepad1.right_stick_x * DT_SPEED;
+            telemetry.addLine(String.format("Expulsion RPM : " + wantedRPM));
+            telemetry.update();
 
-            // Mechanisms Controller Controls
-            double intakePow = gamepad2.right_trigger;
-            double rampPow = gamepad2.left_trigger;
-
-            double expulsionPow = gamepad2.a ? -1.0 : 0.0;
-
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x - rx) / denominator;
-            double backLeftPower = (-y + x + rx) / denominator;
-            double frontRightPower = (-y + x - rx) / denominator;
-            double backRightPower = (y + x + rx) / denominator;
-
-            rightExpulsionMotor.setVelocity(EXPULSION_MOTOR_RPM * RPM_to_RPS * BASE_TICKS_PER_REV_DC * EXPULSION_MOTOR_GEARBOX_RATIO * expulsionPow);
-            leftExpulsionMotor.setVelocity(EXPULSION_MOTOR_RPM * RPM_to_RPS * BASE_TICKS_PER_REV_DC * EXPULSION_MOTOR_GEARBOX_RATIO * expulsionPow);
+            rightExpulsionMotor.setVelocity(wantedRPM * RPM_to_RPS * BASE_TICKS_PER_REV_DC * EXPULSION_MOTOR_GEARBOX_RATIO * -1);
+            leftExpulsionMotor.setVelocity(wantedRPM * RPM_to_RPS * BASE_TICKS_PER_REV_DC * EXPULSION_MOTOR_GEARBOX_RATIO * -1);
         }
     }
 }
